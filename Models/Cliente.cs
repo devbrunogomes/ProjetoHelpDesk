55757797b57dacf13719aeb6e728a2d61f03da63
Bruno Gomes <bsgomes16@gmail.com>
2024-10-10 10:31:28 -0300

    remocao de colunas de password

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -18,19 +18,8 @@ public class Cliente {
 	[EmailAddress(ErrorMessage = "O formato do Email é inválido.")]
 	public string Email { get; set; }
 
-	[Required(ErrorMessage = "A confirmação do Email é obrigatória.")]
-	[Compare("Email", ErrorMessage = "Os Emails não coincidem.")]
-	public string EmailConfirmation { get; set; }
-
 	[Required(ErrorMessage = "O campo Nome de Usuário é obrigatório.")]
 	public string UserName { get; set; }
-
-	[Required(ErrorMessage = "O campo Senha é obrigatório.")]
-	public string Password { get; set; }
-
-	[Required(ErrorMessage = "A confirmação da Senha é obrigatória.")]
-	[Compare("Password", ErrorMessage = "As senhas não coincidem.")]
-	public string RePassword { get; set; }
 
 	[Required(ErrorMessage = "O campo CEP é obrigatório.")]
 	public string Cep { get; set; }
@@ -38,5 +27,5 @@ public class Cliente {
 	public EnumPerfil Perfil { get; set; } //O valor é armazenado como 0, 1 ou 2, dependendo do valor do Enum
 
 	public virtual List<Chamado> Chamados { get; set; } = new List<Chamado>();
-	
+
 }
