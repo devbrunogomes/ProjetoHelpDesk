f04906b8c5206dcb023dd2af6ba38bdf35bd9587
Bruno Gomes <bsgomes16@gmail.com>
2024-10-16 17:13:19 -0300

    integracao com a api de clima concluida

--- Services/ClimaApiService.cs
+++ Services/ClimaApiService.cs
@@ -1,4 +1,5 @@
 ﻿using SolutisHelpDesk.Data.DTOs;
+using System.Runtime.ConstrainedExecution;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -9,11 +10,13 @@ public class ClimaApiService {
 	private TokenService _tokenService;
 	private ClienteService _clienteService;
 	private readonly HttpClient _httpClient;
+	private readonly IConfiguration _configuration;
 
-	public ClimaApiService(TokenService tokenService, ClienteService clienteService, HttpClient httpClient) {
+	public ClimaApiService(TokenService tokenService, ClienteService clienteService, HttpClient httpClient, IConfiguration configuration) {
 		_tokenService = tokenService;
 		_clienteService = clienteService;
 		_httpClient = httpClient;
+		_configuration = configuration;
 	}
 
 	internal async Task ConferirClimaDaRegiao(CreateChamadoDto chamadoDto, ClaimsPrincipal user) {
@@ -22,7 +25,31 @@ public class ClimaApiService {
 		string cep = cliente.Cep;
 
 		//Pegar o municipio do cliente através do via cep
-		string municipio = await GetMunicipioViaCep(cep);      
+		string municipio = await GetMunicipioViaCep(cep); 
+		
+		//Pegar o clima através do municipio
+		string climaRegiao = await GetClimaViaApi(municipio);
+
+	}
+
+	private async Task<string> GetClimaViaApi(string municipio) {
+		string apiKey = _configuration["ClimaApiKey"]!;
+		string url = $"https://api.openweathermap.org/data/2.5/weather?q={municipio},br&appid={apiKey}";
+
+		HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+		if (response.IsSuccessStatusCode) {
+			string jsonResponse = await response.Content.ReadAsStringAsync();
+			var data = JsonSerializer.Deserialize<OpenWeatherResponse>(jsonResponse);
+
+			if (data != null && data.Weather != null && data.Weather.Any()) {
+				return data.Weather[0].Main;
+			} else {
+				throw new Exception("Informação de clima não encontrada.");
+			}
+		} else {
+			throw new Exception("Erro ao acessar o serviço OpenWeather.");
+		}
 	}
 
 	private async Task<string> GetMunicipioViaCep(string cep) {
@@ -44,10 +71,23 @@ public class ClimaApiService {
 		}
 
 	}
+	#region("Classes para desserialização")
+	// Classe auxiliar para deserializar a resposta JSON
+	private class OpenWeatherResponse {
+		[JsonPropertyName("weather")]
+		public List<WeatherInfo> Weather { get; set; }
+	}
+
+	private class WeatherInfo {
+		[JsonPropertyName("main")]
+		public string Main { get; set; }
+	}
 
 	private class ViaCepResponse {
 		[JsonPropertyName("localidade")]
 		public string Localidade { get; set; }
 	}
+
+	#endregion
 }
 
