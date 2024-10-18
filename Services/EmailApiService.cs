12d6febfed59eaddc4768fc7f9124a3222dcf166
Bruno Gomes <bsgomes16@gmail.com>
2024-10-18 11:33:13 -0300

    Inicio da logica do sendGrid

---
+++ Services/EmailApiService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace SolutisHelpDesk.Services;
+
+public class EmailApiService {
+	public async Task EnviarNotificacaoDeAtualizacaoPorEmail(ClaimsPrincipal user) {
+
+	}
+}
