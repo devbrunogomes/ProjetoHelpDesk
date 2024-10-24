bb6b92b723e5c49e4140eb6cf90763e1ddf5be08
Bruno Gomes <bsgomes16@gmail.com>
2024-10-24 14:51:18 -0300

    estilizacao da navbar

--- frontend/src/components/DashboardCliente/DashboardCliente.jsx
+++ frontend/src/components/DashboardCliente/DashboardCliente.jsx
@@ -1,3 +1,25 @@
+import styles from "./styles.module.scss";
+import logo from "../../assets/Solutis_Help_Desk_logo.png";
+
 export const DashboardCliente = (props) => {
-  return <h1>DASHBOARD CLIENTE</h1>;
+  return (
+    <header className={styles.container}>
+      <nav>
+        <img src={logo} alt="Solutis Help Desk" />
+
+        <div>
+          <ul>
+            <li>Dashboard</li>
+            <li>Meus Chamados</li>
+          </ul>
+        </div>
+        <div>
+          <span>
+            <h4>maximusS2</h4>
+          </span>
+          <button>Logout</button>
+        </div>
+      </nav>
+    </header>
+  );
 };
