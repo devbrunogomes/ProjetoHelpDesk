5b43a1647237b3a915df44e3b212d9102d592b16
Bruno Gomes <bsgomes16@gmail.com>
2024-10-25 14:21:17 -0300

    corpo de criacao de chamado

--- frontend/src/components/DashboardCliente/DashboardCliente.jsx
+++ frontend/src/components/DashboardCliente/DashboardCliente.jsx
@@ -4,6 +4,30 @@ import { Navbar } from "../Navbar/Navbar";
 
 export const DashboardCliente = (props) => {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <section >
+          <div>
+            <h1>CRIAR CHAMADO</h1>
+          </div>
+
+          <form action="">
+            <input
+              type="text"
+              placeholder="TÍTULO"
+              required
+            />
+            <textarea
+              type="text"
+              placeholder="Descreva seu problema"
+              required
+            />
+
+            <input type="submit" value="Enviar Chamado" />
+          </form>
+        </section>
+      </div>
+    </>
   );
 };
