2944f63f51443ade01045db943e9c9af5398774c
Bruno Gomes <bsgomes16@gmail.com>
2024-10-25 16:59:19 -0300

    estilizacao de meus chamados pronta

--- frontend/src/components/MeusChamados/MeusChamados.jsx
+++ frontend/src/components/MeusChamados/MeusChamados.jsx
@@ -3,27 +3,33 @@ import styles from "./styles.module.scss";
 export const MeusChamados = (props) => {
   return (
     <div className={styles.container}>
-        <div>
-          <h1>MEUS CHAMADOS</h1>
+      <div>
+        <h1>MEUS CHAMADOS</h1>
+      </div>
+      <section>
+        <div className={styles.titulo}>
+          <h2>Chamado #01</h2>
+          <h2>Em Andamento</h2>
+          <h2>Baixa Prioridade</h2>
         </div>
-        <section>
-          <div >
-            <h2>Chamado #01</h2>
-          </div>
 
-          <div className={styles.titulo}>
-            <span>27/10/2024</span>
-            <span>Título</span>
-            <span>Em Andamento</span>
-            <span>Baixa Prioridade</span>
-          </div>
-          <div>
-            <p>Descrição do Chamado</p>
-          </div>
+        <div className={styles.subTitulo}>
+          <span>27/10/2024</span>
+          <span>Internet caindo toda hora</span>
+        </div>
+        <div className={styles.descricao}>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+            nisi accusantium doloremque quisquam laboriosam vero pariatur
+            maiores dolore fuga. Animi reiciendis natus ipsum aperiam quidem
+            voluptates dolor, facilis ex perspiciatis.
+          </p>
+        </div>
+        <div className={styles.respostasContainer}>
           <div>
             <h3>Respostas</h3>
           </div>
-          <div>
+          <div className={styles.respostas}>
             <div>
               <span>28/10/2024 -</span>
               <span> Autor</span>
@@ -51,7 +57,8 @@ export const MeusChamados = (props) => {
             ></textarea>
             <input type="submit" value="Enviar Resposta" />
           </form>
-        </section>
-      </div>
-  )
-}
+        </div>
+      </section>
+    </div>
+  );
+};
