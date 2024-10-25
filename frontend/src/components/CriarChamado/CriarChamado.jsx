d2de76fe59b3878556b3331573e47525b9dd8fe5
Bruno Gomes <bsgomes16@gmail.com>
2024-10-25 14:58:29 -0300

    logica pra criar um chamado

---
+++ frontend/src/components/CriarChamado/CriarChamado.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import styles from "./styles.module.scss";
+import axios from "axios";
+
+export const CriarChamado = (props) => {
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const token = localStorage.getItem("token");
+
+    const chamadoData = {
+      titulo: titulo,
+      descricao: descricao,
+    };
+
+    console.log(chamadoData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5089/Chamado",
+        chamadoData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      if (error.response) {
+        console.error("Erro ao criar chamado:", error.response.data);
+      } else {
+        console.error("Erro ao criar chamado:", error);
+      }
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <section>
+        <div>
+          <h1>CRIAR CHAMADO</h1>
+        </div>
+
+        <form action="post" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="TÍTULO"
+            value={titulo}
+            onChange={(e) => setTitulo(e.target.value)}
+            required
+          />
+          <textarea
+            type="text"
+            placeholder="Descreva seu problema"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+            required
+          />
+
+          <input type="submit" value="Enviar Chamado" />
+        </form>
+      </section>
+    </div>
+  );
+};
--- frontend/src/components/DashboardCliente/DashboardCliente.jsx
+++ frontend/src/components/DashboardCliente/DashboardCliente.jsx
@@ -1,33 +1,13 @@
 import styles from "./styles.module.scss";
-import logo from "../../assets/Solutis_Help_Desk_logo.png";
+
 import { Navbar } from "../Navbar/Navbar";
+import { CriarChamado } from "../CriarChamado/CriarChamado";
 
 export const DashboardCliente = (props) => {
   return (
     <>
       <Navbar />
-      <div className={styles.container}>
-        <section >
-          <div>
-            <h1>CRIAR CHAMADO</h1>
-          </div>
-
-          <form action="">
-            <input
-              type="text"
-              placeholder="TÍTULO"
-              required
-            />
-            <textarea
-              type="text"
-              placeholder="Descreva seu problema"
-              required
-            />
-
-            <input type="submit" value="Enviar Chamado" />
-          </form>
-        </section>
-      </div>
+      <CriarChamado/>
     </>
   );
 };
