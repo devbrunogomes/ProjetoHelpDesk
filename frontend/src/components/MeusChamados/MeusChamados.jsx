d6b3a528f54e78a92c67a93748763b4204300820
Bruno Gomes <bsgomes16@gmail.com>
2024-10-28 14:54:35 -0300

    refatoracao de resposta como um componente

--- frontend/src/components/MeusChamados/MeusChamados.jsx
+++ frontend/src/components/MeusChamados/MeusChamados.jsx
@@ -1,3 +1,4 @@
+import { Resposta } from "../Resposta/Resposta";
 import styles from "./styles.module.scss";
 
 export const MeusChamados = (props) => {
@@ -25,39 +26,7 @@ export const MeusChamados = (props) => {
             voluptates dolor, facilis ex perspiciatis.
           </p>
         </div>
-        <div className={styles.respostasContainer}>
-          <div>
-            <h3>Respostas</h3>
-          </div>
-          <div className={styles.respostas}>
-            <div>
-              <span>28/10/2024 -</span>
-              <span> Autor</span>
-            </div>
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Distinctio optio necessitatibus exercitationem totam possimus
-                labore illo accusamus maiores. Vitae officiis tenetur debitis
-                distinctio corrupti dignissimos doloremque consequatur tempora
-                temporibus neque!
-              </p>
-            </div>
-          </div>
-          <div>
-            <h3>Reponder</h3>
-          </div>
-          <form action="post">
-            <textarea
-              name="resposta"
-              id="resposta"
-              cols="50"
-              rows="2"
-              required
-            ></textarea>
-            <input type="submit" value="Enviar Resposta" />
-          </form>
-        </div>
+        <Resposta/>
       </section>
     </div>
   );
