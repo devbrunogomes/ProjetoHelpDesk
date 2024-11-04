e6febe3ed5d4fb80cc7400c1cd7767bdffc60e82
Bruno Gomes <bsgomes16@gmail.com>
2024-11-04 15:05:00 -0300

    lista de técnicos com metodo de exclusao

--- frontend/src/components/DashboardAdm/DashboardAdm.jsx
+++ frontend/src/components/DashboardAdm/DashboardAdm.jsx
@@ -1,12 +1,14 @@
 import styles from "./styles.module.scss";
 import { Navbar } from "../Navbar/Navbar";
 import { CadastroTecnicoAdm } from "../CadastroTecnicoAdm/CadastroTecnicoAdm";
+import { ListaTecnicos } from "../ListaTecnicos/ListaTecnicos";
 
 export const DashboardAdm = (props) => {
   return (
     <div className={styles.container}>
       <Navbar />
       <CadastroTecnicoAdm/>
+      <ListaTecnicos/>
     </div>
   );
 };
