01e432ca20eaba03632d1012ae620b004eee5356
Bruno Gomes <bsgomes16@gmail.com>
2024-11-05 11:41:22 -0300

    dados para o gráfico sendo puxados da api corretamente

--- frontend/src/components/GraficoChamados/GraficoChamados.jsx
+++ frontend/src/components/GraficoChamados/GraficoChamados.jsx
@@ -1,13 +1,43 @@
 import styles from "./styles.module.scss";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import { useEffect, useState } from "react";
+import Chart from 'chart.js/auto';
+import axios from "axios";
 
 export const GraficoChamados = (props) => {
   const [isContentVisible, setIsContentVisible] = useState(false);
+  const [chartData, setChartData] = useState(null);
+
+  
 
   const toggleContentVisibility = () => {
     setIsContentVisible(!isContentVisible); // Alterna visibilidade
   };
+
+  async function getChartData() {
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await axios.get("http://localhost:5089/Chamado/chamados-dashboard", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      
+      setChartData(response.data)
+      console.log(chartData)
+    } catch (error) {
+      setChartData(null) 
+      console.error("Erro ao carregar dados do gráfico" + error)
+    }
+
+  }
+
+  useEffect(() => {
+    getChartData();
+    console.log(chartData)
+  }, []);
 
   return (
     <div className={styles.container}>
