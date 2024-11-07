5316a372b82aec49b90cd7573e033685ad2b9318
Bruno Gomes <bsgomes16@gmail.com>
2024-11-07 20:32:43 -0300

    Componente Listar Chamados funcionando corretamente

--- frontend/src/components/ListaChamados/ListaChamados.jsx
+++ frontend/src/components/ListaChamados/ListaChamados.jsx
@@ -1,7 +1,8 @@
 import styles from "./styles.module.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { Chamado } from "../Chamado/Chamado";
 
 export const ListaChamados = (props) => {
   const [isContentVisible, setIsContentVisible] = useState(true);
@@ -15,21 +16,27 @@ export const ListaChamados = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const token = localStorage.getItem("token");
-    
-    if (chamados.length === 0) {
-      try {
-        const response = await axios.get("http://localhost:5089/Chamado", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        
-        console.log(response.data)
-        setChamados(response.data);
-      } catch (error) {
-        console.error("Erro ao consultar chamados: " + error);
-      }
+
+    try {
+      const response = await axios.get("http://localhost:5089/Chamado", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log(response.data);
+      aplicarFiltro(response.data);
+    } catch (error) {
+      console.error("Erro ao consultar chamados: " + error);
     }
+  };
+
+  const aplicarFiltro = (data) => {
+    const chamadosFiltrados = data.filter(
+      (chamado) => chamado.status === parseInt(filtroChamado)
+    );
+
+    setChamados(chamadosFiltrados);
   };
 
   return (
@@ -42,13 +49,24 @@ export const ListaChamados = (props) => {
         <div className={styles.content}>
           <form action="post" onSubmit={handleSubmit}>
             <label htmlFor="filter">Filtro por Status de Chamado</label>
-            <select name="filter" id="filterChamados">
+            <select
+              name="filter"
+              id="filterChamados"
+              value={filtroChamado}
+              onChange={(e) => setFiltroChamado(e.target.value)}
+            >
+              <option value="">--</option>
               <option value="0">Abertos</option>
               <option value="1">Em Andamento</option>
               <option value="2">Fechados</option>
             </select>
             <input type="submit" value="Pesquisar" />
           </form>
+          <div className={styles.chamados}>
+            {chamados.map((chamado) => (
+              <Chamado key={chamado.chamadoId} chamado={chamado} />
+            ))}
+          </div>
         </div>
       )}
     </div>
